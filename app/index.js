84cdc44420d8a7bde06b3358b42c09f9f8347a24
Lucy Barker <lucy@geniemouse.com>
2019-06-03 10:26:13 +0100

    Wrap end task in skip-welcome-message check

--- app/index.js
+++ app/index.js
@@ -133,11 +133,14 @@ module.exports = class extends YeomanGenerator {
 
     // Tidy-up
     end() {
-        this.log(
+        /* istanbul ignore else  */
+        if (!this.options["skip-welcome-message"]) {
+            this.log(
             yosay(
                 `${chalk.blue("Finished generating base project files")}
                 See the ${chalk.bold.italic("README.md")} file further details\n`
             )
         );
+        }
     }
 };
